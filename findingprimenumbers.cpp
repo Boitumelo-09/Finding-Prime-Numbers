2cf238ef2697877993ee7d690ac0e351eae71d2d
Boitumelo-09 <belmiredub4@gmail.com>
2025-02-09 20:06:19 +0200

    FPN v 2.0

--- findingprimenumbers.cpp
+++ findingprimenumbers.cpp
@@ -1,53 +1,60 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
-
-
-bool isPrime(int n) {
-    if (n <= 1) return false;
-    if (n <= 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-    }
-    return true;
-}
-
-// Function to generate prime numbers up to a given limit
-void generatePrimes(int limit) {
-    cout << "Prime numbers up to " << limit << ": ";
-    for (int i = 2; i <= limit; i++) {
-        if (isPrime(i)) {
-            cout << i << " ";
-        }
-    }
-    cout << endl;
-}
-
-int main() {
+  int main(){
+    int low, high, index;
+    bool isPrime_Flag;
+    char option;
     system("cls");
-    int choice, num;
-    cout << "Prime Number Program\n";
-    cout << "1. Check if a number is prime\n";
-    cout << "2. Generate prime numbers up to a limit\n";
-    cout << "Enter your choice: ";
-    cin >> choice;
-
-    if (choice == 1) {
-        cout << "Enter a number: ";
-        cin >> num;
-        if (isPrime(num))
-            cout << num << " is a prime number." << endl;
-        else
-            cout << num << " is not a prime number." << endl;
-    } else if (choice == 2) {
-        cout << "Enter the limit: ";
-        cin >> num;
-        generatePrimes(num);
-    } else {
-        cout << "Invalid choice!" << endl;
-    }
-    return 0;
-}
+    std::cout<<"We're Doing Prime Numbers Today"<<std::endl;
+    std::cout<<"_________________________"<<std::endl;
+        do
+        {
+            do
+            {
+               std::cout<<"Enter the lower limit:";
+           std::cin>>low;
+       system("cls");
+       std::cout<<"Enter the higher limit:";
+           std::cin>>high;
+       system("cls");
+         std::cout<<"_________________________"<<std::endl;
+         std::cout<<"Prime numbers from "<<low<<" to "<<high<<" are:"<<std::endl;
+         while (low<high)
+         {
+            isPrime_Flag=true;
+          if (low==1 || low ==0){
+           isPrime_Flag=false;
+          }
+   
+         
+           for (index =2; index < low/2; index++)
+           {
+               if (low%index==0)
+               {
+                   isPrime_Flag=false;
+                   break;
+               }
+               
+           }
+           if (isPrime_Flag)
+           {
+               std::cout<<low<<std::endl;
+           }
+           ++low;
+        }
+           std::cout<<"_________________________"<<std::endl;
+           std::cout<<"Would you like to run another test?[Y/N]:";
+               std::cin>>option;
+               system("cls");
+        std::cout<<"_________________________"<<std::endl; 
+            } while (option=='Y' || option=='y');
+            
+              if (option=='N' || option=='n')
+              {
+               std::cout<<"Okay Thank You, Bye";
+              }
+              
+              std::cout<<"_________________________"<<std::endl;  
+        } while (option!='N' || option!='n' || option!='Y' || option!='y');
+    system("pause");
+  }
